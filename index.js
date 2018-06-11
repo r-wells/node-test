ac510e5d89ac9b1f1f20b062ed38ff662d339b61
Ryan <r.l.wells@outlook.com>
2018-06-11 12:05:38 -0700

    added module.exports

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 var rect = require('./rectangle');
 
 function solveRect(l, b) {
-    console.log("Solving for rectangle with l = " + l + " and b = " + b);
 
-    if ( l <= 0 || b <= 0 ) {
-        console.log("Rectangle dimensions should be greater than zero");
-    } else {
-        console.log("the area of the rectangle is " + rect.area(l,b));
-        console.log("the perimeter of the rectangle is " + rect.perimeter(l,b));
-    }
+    rect(l, b, (err, rectangle) => {
+        if (err) {
+            console.log("Error: ", err.message);
+        } else {
+            console.log("The area is: " + rectangle.area());
+            console.log("The perimeter is: " + rectangle.perimeter());
+        }
+    });
+    console.log("This statement is after the call to the rectangle");
 }
 
 solveRect(2,4);
